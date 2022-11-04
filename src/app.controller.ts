7efd602f96882169d664470cf144aa65c60988a6
Mario Montero <marioupc3@gmail.com>
2022-11-03 23:02:27 -0500

    routes with params nest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+
+interface UserInterface {
+  id?: number;
+  name: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/hello/:name')
+  getHello2(@Param() params:UserInterface ): string {
+    return this.appService.getHello(params.name);
   }
 }
